7e365f1e5f352ebd5cc02dbd696a3a5efbe1959b
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-02-10 22:10:45 -0300

    revert to local env working

--- root/js/app.js
+++ root/js/app.js
@@ -3,16 +3,20 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let boardCountarr = [];
 let moves = 0;
+let congratsStore = moves;
+let levelOrigin = window.location.href;
+
+let params = new URLSearchParams(window.location.search);
 
 const reset = document.querySelector('.resetbtn')
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-
 reset.addEventListener('click', function () {
-  location.reload();
+  window.location.reload()
   resetBoard();
-})
+});
 
 function counter() {
   moves += 1;
@@ -22,6 +26,36 @@ function counter() {
     document.querySelector('.moves').innerHTML = `${moves} movimentos`
   }
 }
+
+function boardCount() {
+  boardCountarr.push(firstCard, secondCard)
+}
+
+function setUrl() {
+  params.set('moves', moves);
+  if (levelOrigin === 'http://127.0.0.1:5500/root/easy.html')
+    params.append('level', 'easy');
+} if (levelOrigin === 'http://127.0.0.1:5500/root/normal.html') {
+  params.append('level', 'normal');
+} if (levelOrigin === 'http://127.0.0.1:5500/root/hard.html') {
+  params.append('level', 'hard');
+}
+
+function pageRedirect() {
+  if (levelOrigin === 'http://127.0.0.1:5500/root/easy.html' && boardCountarr.length === 4) {
+    setUrl()
+    window.location.href = `${window.location.origin}/root/congrats.html?${params}`;
+  } if (levelOrigin === 'http://127.0.0.1:5500/root/normal.html' && boardCountarr.length === 8) {
+    setUrl()
+    window.location.href = `${window.location.origin}/root/congrats.html?${params}`;
+
+  } if (levelOrigin === 'http://127.0.0.1:5500/root/hard.html' && boardCountarr.length === 16) {
+    setUrl()
+    window.location.href = `${window.location.origin}/root/congrats.html?${params}`;
+
+  }
+}
+
 
 function flipCard() {
 
@@ -42,14 +76,16 @@ function flipCard() {
   checkForMatch();
 }
 
-// Check for match and add background to reflect match or not
+// Check for match and add background to reflect match or show not matching
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
   counter();
+  pageRedirect();
   if (isMatch === true) {
     firstCard.classList.add('correct')
     secondCard.classList.add('correct')
     disableCards()
+    pageRedirect()
   } if (isMatch === false) {
     firstCard.classList.add('wrong')
     secondCard.classList.add('wrong')
@@ -66,6 +102,7 @@ function disableCards() {
   secondCard.removeEventListener('click', flipCard);
   firstCard.classList.remove('bgcolor')
   secondCard.classList.remove('bgcolor')
+  boardCount();
   resetBoard();
 }
 
@@ -80,19 +117,24 @@ function unflipCards() {
     firstCard.classList.remove('bgcolor')
     secondCard.classList.remove('bgcolor')
     resetBoard();
-  }, 1500);
+  }, 500);
 }
 
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
-
 }
+function removeParams() {
+  params.delete('moves')
+  params.delete('level')
+  history.replaceState(null, null, params)
+}
+
 
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 16);
-    card.style.order = randomPos;
+    card.style.order = randomPos
   });
 })();
 
@@ -104,4 +146,3 @@ function setMoves() {
   document.querySelector('.moves').innerHTML = `${moves}`;
   console.log(moves)
 }
-
